1aef98a267ffc997a6d6f67c056b5bb5b70c3c7f
Miguel <miguelpercham@gmail.com>
2020-02-15 12:29:32 -0600

    Primer commit

---
+++ IDE arduino/Sensor_PIR/Sensor_PIR.ino
@@ -0,0 +1,29 @@
+const int LEDPin = 13;        // pin para el LED
+const int PIRPin = 2;
+const int pinBuzzer = 8;  // pin de entrada (for PIR sensor)
+ 
+void setup() 
+{
+   pinMode(LEDPin, OUTPUT); 
+   pinMode(PIRPin, INPUT);
+   Serial.begin(9600);
+}
+ 
+void loop()
+{
+  int value= digitalRead(PIRPin);
+ 
+  if (value == HIGH)
+  {
+    digitalWrite(LEDPin, HIGH);
+    delay(100);
+    digitalWrite(LEDPin, LOW);
+    delay(100);
+    tone(pinBuzzer, 440);
+  }
+  else
+  {
+    digitalWrite(LEDPin, LOW);
+    noTone(pinBuzzer);
+  }
+}
